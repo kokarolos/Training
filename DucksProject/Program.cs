835c441c1e5ea3de92f415572260201cd916b40d
kokarolos <kokarolos@gmail.com>
2020-08-07 11:33:03 +0300

    DuckCall Implementation

--- DucksProject/Program.cs
+++ DucksProject/Program.cs
@@ -9,6 +9,17 @@
             d1.SetFlyingMethod(new FlyingWithWings());
             d1.Quack();
             d1.Fly();
+
+            Duck d2 = new DuckCall();
+            d2.SetQuackingMethod(new QuackingLoud());
+            d2.SetFlyingMethod(new FlyNoWay());
+            d2.Quack();
+            d2.Fly();
+        }
+
+        public class DuckCall : Duck
+        {
+            public override float Weigth => 2.5f;
         }
     }
 }
