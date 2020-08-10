8c3b2cd7e5b13e01e094b22f4e857b0d4a26bb69
panosrizos <pnsrzs@gmail.com>
2020-08-10 16:57:08 +0300

    ObserverPattern

---
+++ ObserverPattern/ObserverPattern/Concrete Classes/ForecastDisplay.cs
@@ -0,0 +1,35 @@
+﻿using ObserverPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern.Concrete_Classes
+{
+    class ForecastDisplay : Observer, DisplayElement
+    {
+        private float temperature;
+        private float humitidy;
+        private Subject weatherData;
+
+        public ForecastDisplay(Subject weatherData)
+        {
+            this.weatherData = weatherData;
+            weatherData.RegisterObserver(this);
+
+        }
+
+        public void Update(float temperature, float humitidy, float pressure)
+        {
+            this.temperature = temperature;
+            this.humitidy = humitidy;
+            Display();
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"F: {temperature} F Deegres and humitidy {humitidy}%");
+        }
+    }
+}
