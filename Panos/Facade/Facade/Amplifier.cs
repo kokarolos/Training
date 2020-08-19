8d3a947ad90568373639d004230833b1510b6c65
panosrizos <pnsrzs@gmail.com>
2020-08-19 16:51:54 +0300

    Facade

---
+++ Panos/Facade/Facade/Amplifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Facade
+{
+    public class Amplifier
+    {
+        public void On()
+        {
+            Console.WriteLine("Amplifier is on");
+        }
+
+        public void SetDvd(DvdPlayer dvd)
+        {
+            Console.WriteLine($"{dvd} is in");
+        }
+
+        public void setSurroundSound()
+        {
+            Console.WriteLine("Sourround Sound is set");
+        }
+
+        public void Off()
+        {
+            Console.WriteLine("Amplifier is Off");
+        }
+    }
+}
---
+++ Panos/Facade/Facade/HomeTheatreFacade.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Facade
+{
+    public class HomeTheatreFacade
+    {
+        Amplifier amp;
+        Tuner tuner;
+        DvdPlayer dvd;
+        CdPlayer cd;
+        Projector projector;
+        TheaterLights lights;
+        Screen screen;
+        PopcornPopper popper;
+
+        public HomeTheatreFacade(Amplifier amp, Tuner tuner, DvdPlayer dvd,
+                                 CdPlayer cd, Projector projector, TheaterLights lights,
+                                 Screen screen, PopcornPopper popper)
+        {
+            this.amp = amp;
+            this.tuner = tuner;
+            this.dvd = dvd;
+            this.cd = cd;
+            this.projector = projector;
+            this.lights = lights;
+            this.screen = screen;
+            this.popper = popper;
+        }
+
+        public void WatchMovie(string movie)
+        {
+            Console.WriteLine("Get ready to watch a movie");
+            popper.On();
+            popper.Pop();
+            lights.Dim(10);
+            screen.Down();
+            projector.On();
+            projector.WideScreenMode();
+            amp.On();
+            amp.SetDvd(dvd);
+            amp.setSurroundSound();
+            dvd.On();
+            dvd.play(movie);
+        }
+
+
+        public void EndMovie()
+        {
+            Console.WriteLine("Shutting movie theater down");
+            popper.Off();
+            lights.On();
+            screen.Up();
+            projector.Off();
+            amp.Off();
+            dvd.Stop();
+            dvd.Eject();
+            dvd.Off();
+        }
+
+    }
+}
