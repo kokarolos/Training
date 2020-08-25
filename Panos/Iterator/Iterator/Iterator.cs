f9f5d570b00533f10fb235e4687e0ad8eb0d4e5f
panosrizos <pnsrzs@gmail.com>
2020-08-25 16:56:59 +0300

    Iterator

---
+++ Panos/Iterator/Iterator/Iterator.cs
@@ -0,0 +1,9 @@
+﻿namespace Iterator
+{
+    public interface Iterator
+    {
+        object Next();
+        bool HasNext();
+    }
+
+}
---
+++ Panos/Iterator/Iterator/PancakeHouseIterator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Iterator
+{
+    public class PancakeHouseIterator : Iterator
+    {
+        List<MenuItem> items;
+        private int position = 0;
+
+        public PancakeHouseIterator(List<MenuItem>items)
+        {
+            this.items = items;
+        }
+
+        public bool HasNext()
+        {
+            return position <= items.Count && !(items[position] is null);
+        }
+
+        public object Next()
+        {
+            var menuItem = items[position];
+            position++;
+            return menuItem;
+        }
+
+
+
+       
+    }
+
+
+}
