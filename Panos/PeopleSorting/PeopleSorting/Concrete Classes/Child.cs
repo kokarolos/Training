275d52459a28bcf94ca3201265094d2a5a641872
panosrizos <pnsrzs@gmail.com>
2020-08-26 13:06:40 +0300

    sorting

---
+++ Panos/PeopleSorting/PeopleSorting/Concrete Classes/Child.cs
@@ -0,0 +1,13 @@
+﻿namespace PeopleSorting
+{
+    public class Child : ISortable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Child(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Panos/PeopleSorting/PeopleSorting/Sorting Methods/BubbleSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeopleSorting
+{
+    public class BubbleSort
+    {
+        /// <summary>
+        /// Method that is sorting an Isortable list
+        /// </summary>
+        /// <param name="list"></param>
+        public void Sort(List<ISortable>list)
+        {
+            ISortable temp;
+            for (int j = 0; j <= list.Count -2; j++)
+            {
+                for (int i = 0; i <= list.Count - 2; i++)
+                {
+                    if (list[i].Id > list[i + 1].Id)
+                    {
+                        temp = list[i+1];
+                        list[i + 1] = list[i];
+                        list[i] = temp;
+                    }
+                }
+            }
+        }
+
+        public static void PrintSortedList(List<ISortable> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.Id);
+            }
+        }
+    }
+}
