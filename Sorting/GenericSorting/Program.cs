4fab78e083386cdf9b2ba940087a3df46a7effa4
kokarolos <kokarolos@gmail.com>
2020-08-27 14:20:54 +0300

    Generic QuickSort implementation

--- Sorting/GenericSorting/Program.cs
+++ Sorting/GenericSorting/Program.cs
@@ -6,19 +6,17 @@ namespace GenericSorting
 {
     class Program
     {
-        //TODO : Turn Void To IEnumerable<T> 
         static void Main(string[] args)
         {
-            EmployeesSort employeesSort = new EmployeesSort();
-            var emps = employeesSort.Sort();
-            Console.WriteLine("Print Sort");
-            foreach (var item in emps)
+            EmployessQuickSort emps = new EmployessQuickSort();
+            var toSort = emps.Sort();
+            foreach (var item in toSort)
             {
-                Console.WriteLine(item.ToString());
+                Console.WriteLine(item);
             }
-            Console.ReadLine();
         }
     }
+
     public abstract class BubbleSort<T>
     {
         protected abstract IComparer<T> GetComparer();
@@ -32,7 +30,7 @@ namespace GenericSorting
             {
                 for (int i = 0; i <= emps.Length - 2; i++)
                 {
-                    IComparer<T> comparer = this.GetComparer();
+                    IComparer<T> comparer = GetComparer();
                     if (comparer.Compare(emps[i], emps[i + 1]) == 1)
                     {
                         temp = emps[i + 1];
@@ -43,76 +41,85 @@ namespace GenericSorting
             }
             return emps;
         }
-       
     }
 
-    public abstract class QuickSort<T>
+    public abstract class QuickShort<T>
     {
         protected abstract IComparer<T> GetComparer();
         protected abstract IEnumerable<T> GetSource();
+
         public IEnumerable<T> Sort()
         {
-            T[] emps = GetSource().ToArray();
-            IComparer<T> comparer = GetComparer();
-            return Sort(emps, 0, emps.Length - 1, comparer);
+            T[] array = GetSource().ToArray();
+            var comparer = GetComparer();
+            Sort(array, 0, array.Length - 1, comparer);
+            return array;
         }
 
-        //left ->first
-        //right -> last
-        //IEnumerable -> GetEnumerator? How to itterate through Ienumerable;
-        private IEnumerable<T> Sort(IEnumerable<T> emps, int left, int right, IComparer<T> comparer)
+        private static void Sort(T[] array, int left, int right, IComparer<T> comparer)
         {
-            int leftIndex = left;
-            int rightIndex = right;
-            T currentEmployee = emps.ElementAt(left);
-            T nextEmployee = emps.ElementAt(right);
-            List<T> tempList = new List<T>();
-
-            //Already Sorted
-            if (emps is null || emps.Count() == 1)
+            int i = left;
+            int j = right;
+            T pivot = array[left + (right - left) / 2];
+            while (i <= j)
             {
-                return emps;
-            }
-
-            T pivot = emps.ElementAt(left + (right - left) / 2);
-            while (left <= right)
-            {
-                while (comparer.Compare(currentEmployee, pivot) == -1)
+                while (comparer.Compare(array[i], pivot) == -1)
                 {
-                    left++;
+                    i++;
                 }
 
-                while (comparer.Compare(nextEmployee, pivot) == 1)
+                while (comparer.Compare(array[j], pivot) == 1)
                 {
-                    right--;
+                    j--;
                 }
 
-                if (left <= right)
+                if (i <= j)
                 {
+                    var temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
 
-                    T temp = emps.ElementAt(left);
-                    tempList = emps.ToList();
-                    tempList[left] = emps.ElementAt(right);
-                    tempList[right] = temp;
-
-                    left++;
-                    right--;
+                    i++;
+                    j--;
                 }
-
-                if (left < right)
+                if (left < j)
                 {
-                    Sort(emps, left, rightIndex, comparer);
+                    Sort(array, left, j, comparer);
                 }
-                if (left < right)
+                if (i < right)
                 {
-                    Sort(emps, leftIndex, right, comparer);
+                    Sort(array, i, right, comparer);
                 }
             }
-            return tempList.AsEnumerable();
         }
     }
 
-    public class EmployeesSort : QuickSort<Employee> // <- needs to become more Abstract
+    public class EmployessQuickSort : QuickShort<Employee>
+    {
+        protected override IComparer<Employee> GetComparer()
+        {
+            return new EmployeeCompareWithID();
+        }
+
+        protected override IEnumerable<Employee> GetSource()
+        {
+            return new List<Employee>()
+            {
+                 new Employee() { ID = 1, Firstname = "Efstathios0", Lastname ="Chrysikos0"},
+                 new Employee() { ID = 0, Firstname = "Efstathios1", Lastname ="Chrysikos1"},
+                 new Employee() { ID = 3, Firstname = "Efstathios9", Lastname ="Chrysikos9"},
+                 new Employee() { ID = 9, Firstname = "Efstathios3", Lastname ="Chrysikos3"},
+                 new Employee() { ID = 8, Firstname = "Efstathios8", Lastname ="Chrysikos8"},
+                 new Employee() { ID = 7, Firstname = "Efstathios7", Lastname ="Chrysikos7"},
+                 new Employee() { ID = 6, Firstname = "Efstathios6", Lastname ="Chrysikos6"},
+                 new Employee() { ID = 5, Firstname = "Efstathios5", Lastname ="Chrysikos5"},
+                 new Employee() { ID = 4, Firstname = "Efstathios4", Lastname ="Chrysikos4"},
+                 new Employee() { ID = 2, Firstname = "Efstathios2", Lastname ="Chrysikos2"},
+            };
+        }
+    }
+
+    public class EmployeesSort : BubbleSort<Employee>
     {
         protected override IComparer<Employee> GetComparer()
         {
@@ -135,6 +142,7 @@ namespace GenericSorting
             };
         }
     }
+
     public class EmployeeCompareWithID : IComparer<Employee>
     {
         public int Compare(Employee x, Employee y)
@@ -142,6 +150,7 @@ namespace GenericSorting
             return x.ID.CompareTo(y.ID);
         }
     }
+
     public class EmployeeCompareWithFirstName : IComparer<Employee>
     {
         public int Compare(Employee x, Employee y)
@@ -149,6 +158,7 @@ namespace GenericSorting
             return x.Firstname.CompareTo(y.Firstname);
         }
     }
+
     public class EmployeeCompareWithLastName : IComparer<Employee>
     {
         public int Compare(Employee x, Employee y)
@@ -156,6 +166,7 @@ namespace GenericSorting
             return x.Lastname.CompareTo(y.Lastname);
         }
     }
+
     public class Employee
     {
         public Employee()
--- Sorting/Sorting/Program.cs
+++ Sorting/Sorting/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Sorting
 {
@@ -6,21 +8,127 @@ namespace Sorting
     {
         static void Main(string[] args)
         {
-            ISortable[] employees = new Employee[]
+            NumSorter numSorter = new NumSorter();
+            var sorted = numSorter.Sort();
+            foreach (var item in sorted)
             {
-                new Employee(1,"Karol","Koniewicz"),
-                new Employee(3,"Panos","Rizos"),
-                new Employee(6,"Takis","Makis"),
-                new Employee(8,"Giannis","Paris"),
-                new Employee(2,"Giannis","Paris"),
-                new Employee(4,"Aris","Papadopoulos"),
-                new Employee(5,"Maria","Sakellariou"),
-            };
-            SortingManager.Sort(employees,new BubbleSort());
-            foreach (var employee in employees)
+                Console.WriteLine(item);
+            }
+
+            StringSorter s = new StringSorter();
+            var sortedString = s.Sort();
+            foreach (var item in sortedString)
             {
-                Console.WriteLine(employee.Id);
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+    public abstract class QuickShort<T>
+    {
+        protected abstract IComparer<T> GetComparer();
+        protected abstract IEnumerable<T> GetSource();
+
+        public IEnumerable<T> Sort()
+        {
+            T[] array = GetSource().ToArray();
+            var comparer = GetComparer();
+            Sort(array, 0, array.Length - 1, comparer);
+            return array;
+        }
+
+        private static void Sort(T[] array, int left, int right, IComparer<T> comparer)
+        {
+            int i = left;
+            int j = right;
+            T pivot = array[left + (right - left) / 2];
+            while (i <= j)
+            {
+                while (comparer.Compare(array[i], pivot) == -1)
+                {
+                    i++;
+                }
+
+                while (comparer.Compare(array[j], pivot) == 1)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    var temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+
+                    i++;
+                    j--;
+                }
+                if (left < j)
+                {
+                    Sort(array, left, j, comparer);
+                }
+                if (i < right)
+                {
+                    Sort(array, i, right, comparer);
+                }
             }
         }
     }
+
+    public class NumSorter : QuickShort<int>
+    {
+        protected override IComparer<int> GetComparer()
+        {
+            return new NumberComparer();
+        }
+
+        protected override IEnumerable<int> GetSource()
+        {
+            return new int[] { 2, 5, -4, 11, 0, 18, 22, 67, 51, 1 };
+        }
+
+        public void PrintSorted<T>()
+        {
+            foreach (var item in GetSource())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+    public class StringSorter : QuickShort<string>
+    {
+        protected override IComparer<string> GetComparer()
+        {
+            return new StringComparer();
+        }
+
+        protected override IEnumerable<string> GetSource()
+        {
+            return new string[] { "Karol", "Panos", "Takis", "Akis" };
+        }
+
+        public void PrintSorted<T>()
+        {
+            foreach (var item in GetSource())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    public class NumberComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            return x.CompareTo(y);
+        }
+    }
+    public class StringComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return x.CompareTo(y);
+        }
+    }
+
 }
