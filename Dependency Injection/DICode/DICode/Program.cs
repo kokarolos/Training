9068980a7795dbd0dc64f4272a4834bfd6e06f24
kokarolos <kokarolos@gmail.com>
2020-09-07 15:21:35 +0300

    xUnit - Mocks

--- Dependency Injection/DICode/DICode/Program.cs
+++ Dependency Injection/DICode/DICode/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Runtime.Remoting.Messaging;
+using System.Configuration;
+using System.Threading;
+using Xunit;
+using Moq;
 
 namespace DICode
 {
@@ -6,8 +11,15 @@ namespace DICode
     {
         static void Main(string[] args)
         {
-            var salutation = new Salutation(new ConsoleMessageWriter());
+            // var t = ConfigurationManager.AppSettings["messageWriter"];
+            // var typeName = typeof(ConsoleMessageWriter);
+            // var type = Type.GetType(typeName.ToString(), true);
+            // IMessageWriter writer = (IMessageWriter)Activator.CreateInstance(type);
+            IMessageWriter writer = new SecureMessageWriter(new ConsoleMessageWriter());
+            writer.Write("sadas");
+            var salutation = new Salutation(writer);
             salutation.Exclaim();
+            //salutation.ExclaimWillWriteCorrectMessageToMessageWriter();
         }
     }
     public interface IMessageWriter
@@ -22,6 +34,25 @@ namespace DICode
             Console.WriteLine(content);
         }
     }
+    public class SecureMessageWriter : IMessageWriter
+    {
+        private readonly IMessageWriter _messageWriter;
+
+        public SecureMessageWriter(IMessageWriter messageWriter)
+        {
+            if (messageWriter == null)
+                throw new ArgumentNullException("writer is null");
+            _messageWriter = messageWriter;
+        }
+
+        public void Write(string content)
+        {
+            if (Thread.CurrentPrincipal.Identity.IsAuthenticated)
+            {
+                _messageWriter.Write(content);
+            }
+        }
+    }
 
     public class Salutation
     {
@@ -34,6 +65,15 @@ namespace DICode
         public void Exclaim()
         {
             _messageWriter.Write("Hello DI!");
+        }
+
+        [Fact]
+        public void ExclaimWillWriteCorrectMessageToMessageWriter()
+        {
+            var writerMock = new Mock<IMessageWriter>();
+            var sut = new Salutation(writerMock.Object); //Dynamically created instace of IMessageWriter
+            sut.Exclaim();
+            writerMock.Verify(w => w.Write("Hello DI"));
         }
     }
 }
